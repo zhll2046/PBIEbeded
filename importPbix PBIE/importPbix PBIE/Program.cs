b6aaf38dd44c5ef153b226e2720fe3a0ecb1bf15
EricZhang271 <v-lvzhan@microsoft.com>
2017-07-07 15:26:55 +0800

    add new projects

---
+++ importPbix PBIE/importPbix PBIE/Program.cs
@@ -0,0 +1,115 @@
+﻿using System; 
+using System.Net; 
+//Install-Package Microsoft.IdentityModel.Clients.ActiveDirectory -Version 2.21.301221612
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.IO; 
+
+namespace ConsoleApplication39
+{
+
+    class Program
+    {
+
+        //Step 1 - Replace {client id} with your client app ID. 
+        //To learn how to get a client app ID, see Register a client app (https://msdn.microsoft.com/en-US/library/dn877542.aspx#clientID)
+        private static string clientID = "49df1bc7-db68-4fb4-91c0-6d93f770d1a4";
+
+        //RedirectUri you used when you registered your app.
+        //For a client app, a redirect uri gives AAD more details on the specific application that it will authenticate.
+        private static string redirectUri = "https://login.live.com/oauth20_desktop.srf";
+
+        //Resource Uri for Power BI API
+        private static string resourceUri = "https://analysis.windows.net/powerbi/api";
+
+        //OAuth2 authority Uri
+        private static string authority = "https://login.windows.net/common/oauth2/authorize";
+
+        private static AuthenticationContext authContext = null;
+        private static string token = String.Empty;
+
+        //Uri for Power BI datasets
+        private static string datasetsUri = "https://api.powerbi.com/v1.0/myorg";
+        private static string datasetsBetaUri = "https://api.powerbi.com/beta/myorg";
+
+        //Example dataset name and group name 
+        private static string groupId = "dc581184-a209-463b-8446-5432f16b6c15";
+
+        static void Main(string[] args)
+        {
+            //Import sample 
+            string pbixPath = @"C:\test\2.pbix";
+            string datasetDisplayName = "mydataset";
+
+            string importResponse = Import(string.Format("{0}/groups/{1}/imports?datasetDisplayName={2}", datasetsBetaUri, groupId, datasetDisplayName), pbixPath);
+              
+            Console.WriteLine(importResponse);
+
+        }
+
+        public static string Import(string url, string fileName)
+        {
+            string responseStatusCode = string.Empty;
+
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            request.Method = "POST";
+            request.KeepAlive = true;
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", AccessToken()));
+
+            using (Stream rs = request.GetRequestStream())
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+                string headerTemplate = "Content-Disposition: form-data; filename=\"{0}\"\r\nContent-Type: application / octet - stream\r\n\r\n";
+                string header = string.Format(headerTemplate, fileName);
+                byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+                rs.Write(headerbytes, 0, headerbytes.Length);
+
+                using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead = 0;
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        rs.Write(buffer, 0, bytesRead);
+                    }
+                }
+
+                byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+                rs.Write(trailer, 0, trailer.Length);
+            }
+            using (HttpWebResponse response = request.GetResponse() as System.Net.HttpWebResponse)
+            {
+                responseStatusCode = response.StatusCode.ToString();
+            }
+
+            return responseStatusCode;
+        }
+
+
+        static string AccessToken()
+        {
+            if (token == String.Empty)
+            {
+                //Get Azure access token
+                // Create an instance of TokenCache to cache the access token
+                TokenCache TC = new TokenCache();
+                // Create an instance of AuthenticationContext to acquire an Azure access token
+                authContext = new AuthenticationContext(authority, TC);
+                // Call AcquireToken to get an Azure token from Azure Active Directory token issuance endpoint
+                token = authContext.AcquireToken(resourceUri, clientID, new Uri(redirectUri), PromptBehavior.RefreshSession).AccessToken;
+            }
+            else
+            {
+                // Get the token in the cache
+                token = authContext.AcquireTokenSilent(resourceUri, clientID).AccessToken;
+            }
+
+            return token;
+        } 
+    }
+}
---
+++ pushdata PBIE/pushdata PBIE/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PowerBI.Api.V1;
+using Microsoft.Rest;
+using Microsoft.PowerBI.Api.V1.Models;
+
+namespace ConsoleApplication39
+{
+    class Program
+    {
+
+        static string accesskey = "KJixsmmw+NGNOtjDZTLOMxi8nilQJ+EzvXDRKVLBnP5HFp7G45RYHRIO23ViXY8iE/zNVHROCmcaOIFr6a2vmQ==";
+        static string workspaceCollectionName = "cisdemo";
+        static string workspaceId = "79c71931-de23-49a8-a3c8-b79d2192fe0b";
+        static void Main(string[] args)
+        {
+
+            var credentials = new TokenCredentials(accesskey, "AppKey");
+
+            // Instantiate your Power BI client passing in the required credentials
+            var client = new PowerBIClient(credentials);
+
+            // Override the api endpoint base URL.  Default value is https://api.powerbi.com
+            client.BaseUri = new Uri("https://api.powerbi.com");
+
+            //create a dataset and get datasetkey
+            string datasetID = CreateDatasets(workspaceCollectionName, workspaceId, client);
+
+            string data = @"{  ""rows"":  
+                                [
+                                    { ""id"": 1, ""name"": ""Tom""},                                
+                                    { ""id"": 2, ""name"": ""Jerry""}
+                                ]
+                            }";
+
+            object dataObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Object>(data.ToString());
+
+            PostRows(workspaceCollectionName, workspaceId, datasetID, "testtable", dataObj, client);
+
+        }
+
+
+        static void PostRows(string workspaceCollectionName, string workspaceId, string datasetid, string tableName, object datajson, PowerBIClient client)
+        {
+            //public static object PostRows(this IDatasets operations, string collectionName, string workspaceId, string datasetKey, string tableName, object requestMessage);
+            var response = client.Datasets.PostRows(workspaceCollectionName, workspaceId, datasetid, tableName, datajson);
+
+
+        }
+
+        static string CreateDatasets(string workspaceCollectionName, string workspaceId, PowerBIClient client)
+        {
+
+            Dataset ds = new Dataset();
+            ds.Name = "testdataset";
+            Table table1 = new Table();
+            table1.Name = "testTable";
+
+
+            Column column1 = new Column("id", "Int64");
+            Column column2 = new Column("name", "string");
+
+            table1.Columns = new List<Column>() { column1, column2 };
+
+            ds.Tables = new List<Table>() { table1 };
+
+            var response = client.Datasets.PostDataset(workspaceCollectionName, workspaceId, ds);
+
+            dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Object>(response.ToString());
+            return obj["id"].ToString();
+        }
+
+
+
+    }
+}
